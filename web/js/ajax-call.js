a9d6d3faa73619555867c946558b12cf38930aab
biancalina <bianca.alyna30@gmail.com>
2020-03-11 10:39:28 +0000

    add more content in the ajax-call.js file

--- web/js/ajax-call.js
+++ web/js/ajax-call.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+const getResponse = path => {
+  const ajaxCall = new XMLHttpRequest()
+  ajaxCall.onreadystatechange = () => {
+    if(ajaxCall.readyState === 4 && ajaxCall.status === 200) {
+      const response = JSON.parse(ajaxCall.responseText)
+      console.table(response)
+    }
+  }
+
+  ajaxCall.open('GET', `http://localhost:3344/${path}`)
+  ajaxCall.send()
+}
+
+const keyList = ["animal", "weather", "food"]
+keyList.forEach(key => {
+  getResponse(key)
+})
