50d39a4b131c361c7615b90cfba36fbaf9488e6a
biancalina <bianca.alyna30@gmail.com>
2020-03-12 07:42:43 +0000

    finish all the tables and style them

--- web/js/ajax-call.js
+++ web/js/ajax-call.js
@@ -1,9 +1,11 @@
+const htmlWillRenderHere = document.querySelector('#view')
+
 const getResponse = path => {
   const ajaxCall = new XMLHttpRequest()
   ajaxCall.onreadystatechange = () => {
     if(ajaxCall.readyState === 4 && ajaxCall.status === 200) {
       const response = JSON.parse(ajaxCall.responseText)
-      console.table(response)
+      render(response)
     }
   }
 
@@ -15,3 +17,34 @@ const keyList = ["animal", "weather", "food"]
 keyList.forEach(key => {
   getResponse(key)
 })
+
+const render = response => {
+  console.table(response[0])
+  const keyList = Object.keys(response[0])
+  console.log(keyList)
+  const table = document.createElement('table')
+  const tableHead = document.createElement('thead')
+  const tableHeadRow = document.createElement('tr')
+  const tableBody = document.createElement('tbody')
+  keyList.forEach(key => {
+    const td = document.createElement('td')
+    td.textContent = key
+    tableHeadRow.appendChild(td)
+  })
+  response.forEach(o => {
+    const tableBodyRow = document.createElement('tr')
+    keyList.forEach(key =>{
+      console.log(o[key])
+      const td = document.createElement('td')
+      td.textContent = o[key]
+      tableBodyRow.appendChild(td)
+    })
+    tableBody.appendChild(tableBodyRow)
+  })
+
+  tableHead.appendChild(tableHeadRow)
+  table.appendChild(tableHead)
+  table.appendChild(tableBody)
+  htmlWillRenderHere.appendChild(table)
+  htmlWillRenderHere.className = 'view-one'
+}
